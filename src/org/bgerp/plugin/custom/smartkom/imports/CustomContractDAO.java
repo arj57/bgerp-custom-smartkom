e4cba4041f436ebf44c0b2d214f9ce36c00cacf2
Alexander Yaryzhenko <ay@smartkom.ru>
2024-05-02 11:18:17 +0600

    CustomContractDAO.java - Refactoring

--- src/org/bgerp/plugin/custom/smartkom/imports/CustomContractDAO.java
+++ src/org/bgerp/plugin/custom/smartkom/imports/CustomContractDAO.java
@@ -1,14 +1,54 @@
 package org.bgerp.plugin.custom.smartkom.imports;
 
 import org.bgerp.model.base.IdTitle;
-//import org.json.JSONObject;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
 import ru.bgcrm.model.user.User;
 import ru.bgcrm.plugin.bgbilling.RequestJsonRpc;
 import ru.bgcrm.plugin.bgbilling.proto.dao.ContractDAO;
-//import ru.bgcrm.plugin.bgbilling.proto.dao.version.v8x.ContractDAO8x;
+
+
+/*
+Примерный URL запроса:
+
+http://127.0.0.1:8080/bgbilling/executer/json/ru.bitel.bgbilling.kernel.contract.api/ContractService
+   {"method" : "contractList",
+   "user" :{ "user" : "shamil", "pswd" : "xxxx" },
+   "params" : {
+   "title" : "0",
+   "fc" : -1,
+   "groupMask" : 0,
+   "subContracts" : false,
+   "closed" : true,
+   "hidden" : false,
+   "page" : { "pageIndex" : 2, "pageSize" : 2 }
+   } }
+
+Примерный ответ:
+
+{"status":"ok","message":"",
+   "data":
+   {
+    "page":{"pageSize":2,"pageIndex":2,"pageCount":49,"recordCount":97,"pageFirstRecordNumber":2},
+    "return":
+    [{"id":353023,"title":"0022010","groups":0,"password":"bg2rFZ2PEX","dateFrom":"2010-01-02","dateTo":null,"balanceMode":0,"paramGroupId":14,"personType":0,"comment":"","hidden":false,"superCid":0,"dependSubList":"","status":0,"statusTimeChange":"2010-01-13","titlePatternId":0,"balanceSubMode":0,"sub":false,"independSub":false,"balanceLimit":0.00,"super":false,"dependSub":false},
+    {"id":353209,"title":"06-10-10/И-Г/0","groups":0,"password":"9351220759","dateFrom":"2010-10-06","dateTo":null,"balanceMode":1,"paramGroupId":14,"personType":0,"comment":"","hidden":false,"superCid":0,"dependSubList":"","status":0,"statusTimeChange":"2010-10-06","titlePatternId":0,"balanceSubMode":0,"sub":false,"independSub":false,"balanceLimit":0.00,"super":false,"dependSub":false}]}}
+
+Примеры.
+
+Преобразование в тип:
+ TypeTreeItem childItem = jsonMapper.convertValue(transferData.postDataReturn(req, user), TypeTreeItem.class);
+
+Получение как List:
+ readJsonValue(transferData.postDataReturn(req, user).traverse(),
+                jsonTypeFactory.constructCollectionType( List.class, IdTitle.class ) )
+
+Ссылки:
+ http://www.bgbilling.ru/v6.1/doc/ch02s08.html
+ http://wiki.fasterxml.com/JacksonInFiveMinutes
+
+*/
 
 public class CustomContractDAO extends ContractDAO {
 
@@ -30,11 +70,7 @@ public class CustomContractDAO extends ContractDAO {
         RequestJsonRpc req = new RequestJsonRpc(KERNEL_CONTRACT_API, "ContractService", "contractByTitle");
         req.setParam("contractTitle", title);
         
-        JsonNode res = transferData.postData(req, user);
-        res = res.path("return");
-//        JSONObject jo = new JSONObject("{\"id\":21739,\"title\":\"100978\"}"); // работает
-//        String res = "{\"id\":21739,\"title\":\"100978\",\"groups\":256,\"password\":\"672678\",\"dateFrom\":\"2018-03-27\",\"dateTo\":null,\"balanceMode\":0,\"paramGroupId\":1,\"personType\":1,\"comment\":\"ПАО АКБ \\\"АВАНГАРД\\\"\",\"hidden\":false,\"superCid\":-1,\"dependSubList\":\"21740,21741,21807,21808,21809,21810,21811,21812,21813,21814,21815,21816,21817,21818,21820,21821,21822,21823,23047,23362,23653,23731,23733,23774,23939,24100,25316,27447,28610,29090,29600,29818\",\"status\":0,\"statusTimeChange\":\"2018-03-27\",\"titlePatternId\":1,\"balanceSubMode\":0,\"domainId\":0,\"super\":true,\"dependSub\":false,\"balanceLimit\":0.00,\"sub\":false,\"independSub\":false}";
-//        JSONObject contract = jsonMapper.convertValue(res, JSONObject.class); // null :((
+        JsonNode res = transferData.postDataReturn(req, user);
         JsonNode titleNode = res.get("title");
         if (titleNode != null) {
             int id = res.get("id").asInt();
